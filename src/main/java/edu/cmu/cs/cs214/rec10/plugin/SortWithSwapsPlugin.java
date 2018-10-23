22a81ab43500f677fa3fe00951542133666ee274
jiatiansun <jiatiansun@163.com>
2018-10-23 11:39:00 -0400

    change to be approved

--- src/main/java/edu/cmu/cs/cs214/rec10/plugin/SortWithSwapsPlugin.java
+++ src/main/java/edu/cmu/cs/cs214/rec10/plugin/SortWithSwapsPlugin.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class SortWithSwapsPlugin implements GamePlugin {
     private int ONE = 1;
-    private int TEN = 3;
+    private int TEN = 8;
 
     // Describes whether the current move is over
     public Boolean move = false;
